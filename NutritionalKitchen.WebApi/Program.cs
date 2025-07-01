9ea080ca4cae39be5ac1c065f1926923b0181438
stephani <stephani.hc.97@gmail.com>
2025-07-01 06:45:34 -0400

    adicion de mejora

--- NutritionalKitchen.WebApi/Program.cs
+++ NutritionalKitchen.WebApi/Program.cs
@@ -1,4 +1,4 @@
-
+  
 using NutritionalKitchen.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -50,3 +50,4 @@ app.UseEndpoints(endpoints =>
 app.MapControllers();
 
 app.Run();
+ 
